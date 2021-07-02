e317a93c16f2f0c630e0e08021130fc84af2abbd
zaid070902 <zaidflandorp4@gmail.com>
2021-07-02 16:09:00 +0200

    first commit

---
+++ poke.js
@@ -0,0 +1,52 @@
+let base_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+// Function to fetch a list of pokemon
+function getPokemonList(url) {
+  fetch(url)
+    // Convert data from JSON
+    .then((response) => response.json())
+    //Stuff to do with data
+    .then((data) => {
+      // Console log to make sure I am getting the data
+      console.log(data);
+      // Get the list of pokemon from the results
+      let pokemon = data.results;
+      // Get element from HTML to write buttons in
+      let container = document.querySelector(".pokemon-list-container");
+      // Clear the container
+      container.innerHTML = "";
+      // Loop over pokemon list and create an HTML button for each one. Add the button to the container
+      pokemon.forEach((btn) => {
+        container.innerHTML += `<button onclick="getPokemonInfo('${btn.url}')">${btn.name}</button>`;
+      });
+      // Add a next pokemon button
+      container.innerHTML += `<br><br><button onclick="getPokemonList('${data.next}')">Next</button>`;
+          container.innerHTML += `<button onclick="getPokemonList('${data.previous}')">Prev</button>`;
+    });
+}
+
+// Get default pokemon list
+getPokemonList(base_URL);
+
+// Function to get information about a specific pokemin
+function getPokemonInfo(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // Make sure data comes through
+      console.log(data);
+      // Write data to pokemon information container
+      document.querySelector(".pokeInfo").innerHTML = `
+    <img src="${data.sprites.front_default} ">
+    `;
+     document.querySelector(".pokename").innerHTML = `${data.name}`;
+    
+document.querySelector(".pokeheight").innerHTML = `<p>Height: ${data.height}</p>`;
+    
+document.querySelector(".pokeweight").innerHTML = `<p>Weight: ${data.weight}</p>`;
+    
+document.querySelector(".poketype").innerHTML = `<p>Type: ${data.types[0].type.name}</p>`;
+document.querySelector(".pokeability").innerHTML = `<p>Ability: ${data.abilities[0].ability.name}</p>`;
+    // document.querySelector(".poketype2").innerHTML = `<p>Type: ${data.types[1].type.name}</p>`;
+   });
+}
